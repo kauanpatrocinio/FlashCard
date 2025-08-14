b97b4a401b7a0051588bca5333dba1b9a1f5c636
kauanpatrocinio <134608232+kauanpatrocinio@users.noreply.github.com>
2025-08-14 15:34:17 -0300

    Add files via upload

---
+++ pergutas.js
@@ -0,0 +1,39 @@
+criaCartao(
+    'biologia',
+    'Qual é o maior órgão do corpo humano?',
+    'A pele'
+)
+
+criaCartao(
+    'Biologia',
+    'Qual o órgão mais forte do corpo humano?',
+    'O coracao'
+)
+
+
+criaCartao(
+    'Biologia',
+    'Qual o órgão mais frágil do corpo humano?',
+    'cérebro'
+)
+
+
+criaCartao(
+    'Biologia',
+'Fórmula simplificada da fotossíntese?',
+'6 O26 CO2 + 6 H2O + luz → C6H12O6'
+)
+
+
+criaCartao(
+    'Biologia',
+    'O que é seleção natural?',
+    'Características vantajosas aumentam em frequência.'
+)
+
+
+criaCartao(
+    'Biologia',
+    'O que estuda a ecologia?',  
+    'Interações entre seres vivos e ambiente.'
+)
